9f5de5b0657fc6d5a91fb01d7fb700d3795afbd7
kagankuscu <kuscukagan@gmail.com>
2023-10-09 18:35:31 +0300

    Main View designed and make it folder.

---
+++ books/books/App/booksApp.swift
@@ -0,0 +1,17 @@
+//
+//  booksApp.swift
+//  books
+//
+//  Created by Kagan Kuscu on 9.10.23.
+//
+
+import SwiftUI
+
+@main
+struct booksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
