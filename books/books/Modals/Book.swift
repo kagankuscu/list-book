b9020aea989618e34c5559f7c7b62f329b280d5a
kagankuscu <kuscukagan@gmail.com>
2023-10-10 12:29:31 +0300

    Book Model editing added isRead variable to track book is read or not read

--- books/books/Modals/Book.swift
+++ books/books/Modals/Book.swift
@@ -15,6 +15,7 @@ struct Book: Codable, Identifiable {
     let author: String
     let description: String
     let isLike: Bool
+    let isRead: Bool
 }
 
 extension Book {
@@ -24,84 +25,97 @@ extension Book {
             bookName: "test 1",
             author: "SC",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a sem felis. Aenean velit lacus, vestibulum non accumsan eget, pharetra non elit. Nam ornare euismod mi a rutrum. Curabitur id imperdiet nunc. In placerat eros et vehicula eleifend. Vivamus at massa euismod mi tincidunt cursus a vel lorem. Quisque vestibulum sem quis cursus venenatis. Pellentesque quis augue non enim facilisis mattis. Duis erat odio, eleifend quis massa sit amet, fringilla ultricies lacus. Quisque scelerisque lectus orci, sed ultricies neque aliquam imperdiet. Cras gravida porttitor enim nec feugiat. Phasellus laoreet orci ligula, non aliquet nibh euismod sed. In eleifend non purus vitae cursus. Phasellus fringilla augue eget arcu mollis, id auctor ex rutrum.",
-            isLike: false
+            isLike: false,
+            isRead: false
         ),
         Book(
             image: "placeholder",
             bookName: "test 2",
             author: "SC",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a sem felis. Aenean velit lacus, vestibulum non accumsan eget, pharetra non elit. Nam ornare euismod mi a rutrum. Curabitur id imperdiet nunc. In placerat eros et vehicula eleifend. Vivamus at massa euismod mi tincidunt cursus a vel lorem. Quisque vestibulum sem quis cursus venenatis. Pellentesque quis augue non enim facilisis mattis. Duis erat odio, eleifend quis massa sit amet, fringilla ultricies lacus. Quisque scelerisque lectus orci, sed ultricies neque aliquam imperdiet. Cras gravida porttitor enim nec feugiat. Phasellus laoreet orci ligula, non aliquet nibh euismod sed. In eleifend non purus vitae cursus. Phasellus fringilla augue eget arcu mollis, id auctor ex rutrum.",
-            isLike: true
+            isLike: true,
+            isRead: true
         ),
         Book(
             image: "placeholder",
             bookName: "test 3",
             author: "SC",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a sem felis. Aenean velit lacus, vestibulum non accumsan eget, pharetra non elit. Nam ornare euismod mi a rutrum. Curabitur id imperdiet nunc. In placerat eros et vehicula eleifend. Vivamus at massa euismod mi tincidunt cursus a vel lorem. Quisque vestibulum sem quis cursus venenatis. Pellentesque quis augue non enim facilisis mattis. Duis erat odio, eleifend quis massa sit amet, fringilla ultricies lacus. Quisque scelerisque lectus orci, sed ultricies neque aliquam imperdiet. Cras gravida porttitor enim nec feugiat. Phasellus laoreet orci ligula, non aliquet nibh euismod sed. In eleifend non purus vitae cursus. Phasellus fringilla augue eget arcu mollis, id auctor ex rutrum.",
-            isLike: false
+            isLike: false,
+            isRead: true
         ),
         Book(
             image: "placeholder",
             bookName: "test 4",
             author: "SC",
             description: "Lorem Ipsum",
-            isLike: false
+            isLike: false,
+            isRead: true
         ),
         Book(
             image: "placeholder",
             bookName: "test 5",
             author: "SC",
             description: "Lorem Ipsum",
-            isLike: true
+            isLike: true,
+            isRead: false
         ),
         Book(
             image: "placeholder",
             bookName: "test 6",
             author: "SC",
             description: "Lorem Ipsum",
-            isLike: false
+            isLike: false,
+            isRead: true
         ),
         Book(
             image: "placeholder",
             bookName: "test 7",
             author: "SC",
             description: "Lorem Ipsum",
-            isLike: false
+            isLike: false,
+            isRead: false
         ),
         Book(
             image: "placeholder",
             bookName: "test 8",
             author: "SC",
             description: "Lorem Ipsum",
-            isLike: true
+            isLike: true,
+            isRead: true
         ),
         Book(
             image: "placeholder",
             bookName: "test 9",
             author: "SC",
             description: "Lorem Ipsum",
-            isLike: false
+            isLike: false,
+            isRead: false
         ),
         Book(
             image: "placeholder",
             bookName: "test 10",
             author: "SC",
             description: "Lorem Ipsum",
-            isLike: false
+            isLike: false,
+            isRead: false
         ),
         Book(
             image: "placeholder",
             bookName: "test 11",
             author: "SC",
             description: "Lorem Ipsum",
-            isLike: true
+            isLike: true,
+            isRead: false
         ),
         Book(
             image: "placeholder",
             bookName: "test 12",
             author: "SC",
             description: "Lorem Ipsum",
-            isLike: false
+            isLike: false,
+            isRead: false
         ),
     ]
+    static let mockDataSingle = Book(image: "placeholder", bookName: "PlaceHolder Name", author: "PlaceHolder Author", description: "PlaceHolder Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a sem felis. Aenean velit lacus, vestibulum non accumsan eget, pharetra non elit. Nam ornare euismod mi a rutrum. Curabitur id imperdiet nunc. In placerat eros et vehicula eleifend. Vivamus at massa euismod mi tincidunt cursus a vel lorem. Quisque vestibulum sem quis cursus venenatis. Pellentesque quis augue non enim facilisis mattis. Duis erat odio, eleifend quis massa sit amet, fringilla ultricies lacus. Quisque scelerisque lectus orci, sed ultricies neque aliquam imperdiet. Cras gravida porttitor enim nec feugiat. Phasellus laoreet orci ligula, non aliquet nibh euismod sed. In eleifend non purus vitae cursus. Phasellus fringilla augue eget arcu mollis, id auctor ex rutrum.", isLike: true, isRead: true)
 }
